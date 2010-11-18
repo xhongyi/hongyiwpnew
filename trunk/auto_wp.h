545bc1cd59cac2059da01aa417f009f0853c2835
gohongyi <gohongyi@785ca939-0bf8-b47d-48db-cb9afb3264ec>
2010-11-18 22:55:34 +0000

    to school

--- trunk/auto_wp.h
+++ trunk/auto_wp.h
@@ -8,6 +8,9 @@
 #ifndef AUTO_WP_H_
 #define AUTO_WP_H_
 
+#include <deque>
+#include <iostream>
+
 template<class ADDRESS, class FLAGS>
 struct watchpoint_t {
 	ADDRESS		start_addr;
@@ -21,7 +24,25 @@ public:
 	WatchPoint();
 	~WatchPoint();
 
-	void	add_watch (ADDRESS start_addr, ADDRESS end_addr);
+	void	add_watch	(ADDRESS start_addr, ADDRESS end_addr);
+	void	add_read	(ADDRESS start_addr, ADDRESS end_addr);
+	void	add_write	(ADDRESS start_addr, ADDRESS end_addr);
+
+	void	rm_watch	(ADDRESS start_addr, ADDRESS end_addr);
+	void	rm_read		(ADDRESS start_addr, ADDRESS end_addr);
+	void	rm_write	(ADDRESS start_addr, ADDRESS end_addr);
+
+	bool	watch_fault	(ADDRESS start_addr, ADDRESS end_addr);
+	bool	read_fault		(ADDRESS start_addr, ADDRESS end_addr);
+	bool	write_fault	(ADDRESS start_addr, ADDRESS end_addr);
+
+	void	watch_print();
+private:
+	void	add_watchpoint	(ADDRESS start_addr, ADDRESS end_addr, FLAGS target_flags);
+	void	rm_watchpoint	(ADDRESS start_addr, ADDRESS end_addr, FLAGS target_flags);
+	void	general_fault	(ADDRESS start_addr, ADDRESS end_addr, FLAGS target_flags);
+
+	deque< watchpoint_t<ADDRESS, FLAGS> > wp;
 };
 
 #endif /* AUTO_WP_H_ */
