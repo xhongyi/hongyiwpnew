5c3aad34f8d370d6e62d05cb16a679eb98f8e8c9
gohongyi <gohongyi@785ca939-0bf8-b47d-48db-cb9afb3264ec>
2010-11-18 21:47:38 +0000

    added first file

---
+++ auto_wp.h
@@ -0,0 +1,27 @@
+/*
+ * auto_wp.h
+ *
+ *  Created on: Nov 18, 2010
+ *      Author: xhongyi
+ */
+
+#ifndef AUTO_WP_H_
+#define AUTO_WP_H_
+
+template<class ADDRESS, class FLAGS>
+struct watchpoint_t {
+	ADDRESS		start_addr;
+	ADDRESS		end_addr;
+	FLAGS		flags;
+};
+
+template<class ADDRESS, class FLAGS>
+class WatchPoint {
+public:
+	WatchPoint();
+	~WatchPoint();
+
+	void	add_watch (ADDRESS start_addr, ADDRESS end_addr)
+};
+
+#endif /* AUTO_WP_H_ */
