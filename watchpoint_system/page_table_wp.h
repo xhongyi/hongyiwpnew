61234320bccfb96d79eb8b47a3c21d88cc7599be
camellyx <camellyx@785ca939-0bf8-b47d-48db-cb9afb3264ec>
2011-05-09 17:49:45 +0000

    replacing tabs with spaces

--- watchpoint_system/page_table_wp.h
+++ watchpoint_system/page_table_wp.h
@@ -8,13 +8,13 @@
 #ifndef PAGE_TABLE_WP_H_
 #define PAGE_TABLE_WP_H_
 
-#define	VIRTUAL_ADDRESS_LENGTH	32
-#define	PAGE_OFFSET_LENGTH		12			//page size is 4 KB
-#define	PAGE_NUMBER				(1<<12)
-#define	BIT_MAP_OFFSET_LENGTH	3			//we store one bit for each page in a bit map, 
-											//which is byte addressable
-#define	BIT_MAP_NUMBER_LENGTH	(VIRTUAL_ADDRESS_LENGTH - PAGE_OFFSET_LENGTH - BIT_MAP_OFFSET_LENGTH)
-#define	BIT_MAP_NUMBER			(1<<BIT_MAP_NUMBER_LENGTH)	//needs 128 KB to store these bits
+#define  VIRTUAL_ADDRESS_LENGTH  32
+#define  PAGE_OFFSET_LENGTH      12       //page size is 4 KB
+#define  PAGE_NUMBER             (1<<12)
+#define  BIT_MAP_OFFSET_LENGTH   3        //we store one bit for each page in a bit map, 
+                                          //which is byte addressable
+#define  BIT_MAP_NUMBER_LENGTH   (VIRTUAL_ADDRESS_LENGTH - PAGE_OFFSET_LENGTH - BIT_MAP_OFFSET_LENGTH)
+#define  BIT_MAP_NUMBER          (1<<BIT_MAP_NUMBER_LENGTH) //needs 128 KB to store these bits
 
 #include "oracle_wp.h"
 
@@ -25,42 +25,42 @@ public:
 	~WatchPoint_PT();
 	
 	/*
-	 *	this function tells all pages covered by this range is watched or not
+	 * this function tells all pages covered by this range is watched or not
 	 */
-	bool	watch_fault		(ADDRESS start_addr, ADDRESS end_addr);
+	bool     watch_fault    (ADDRESS start_addr, ADDRESS end_addr);
 	
 	/*
-	 *	we don't need these functions because the page table do not keep 
-	 *	what kind of watchpoint it is having
+	 * we don't need these functions because the page table do not keep 
+	 * what kind of watchpoint it is having
 	 */
 	
-	//bool	read_fault		(ADDRESS start_addr, ADDRESS end_addr);
-	//bool	write_fault		(ADDRESS start_addr, ADDRESS end_addr);
+	//bool   read_fault  (ADDRESS start_addr, ADDRESS end_addr);
+	//bool   write_fault (ADDRESS start_addr, ADDRESS end_addr);
 	
-	void	watch_print(ostream &output = cout);
+	void     watch_print (ostream &output = cout);
 	
 	/*
-	 *	returns the number of changes it does on bit_map
-	 *	for set watchpoint: just call add_watchpoint
-	 *	for update watchpoint: needs to call rm_watchpoint on all ranges and then add_watchpoint to overwrite
-	 *	for counting the number of changes: update = abs(add - rm);
+	 * returns the number of changes it does on bit_map
+	 * for set watchpoint: just call add_watchpoint
+	 * for update watchpoint: needs to call rm_watchpoint on all ranges and then add_watchpoint to overwrite
+	 * for counting the number of changes: update = abs(add - rm);
 	 *
-	 *	add_watchpoint needs target_flags just to check if it is none flag or not
+	 * add_watchpoint needs target_flags just to check if it is none flag or not
 	 */
-	unsigned int	add_watchpoint	(ADDRESS start_addr, ADDRESS end_addr, FLAGS target_flags);
-	unsigned int	rm_watchpoint	(ADDRESS start_addr, ADDRESS end_addr);
+	unsigned int   add_watchpoint (ADDRESS start_addr, ADDRESS end_addr, FLAGS target_flags);
+	unsigned int   rm_watchpoint  (ADDRESS start_addr, ADDRESS end_addr);
 
 //private:
 	/*
-	 *	initialized when constructing
-	 *	used for checking each page's state when rm_watchpoint is called
+	 * initialized when constructing
+	 * used for checking each page's state when rm_watchpoint is called
 	 */
 	Oracle<ADDRESS, FLAGS> *wp;
 	
 	/*
-	 *	one bit for each page
-	 *	keeping track of whether this page is watched or not
-	 *	the software will keep what kind of watchpoint it is
+	 * one bit for each page
+	 * keeping track of whether this page is watched or not
+	 * the software will keep what kind of watchpoint it is
 	 */
 	unsigned char bit_map[BIT_MAP_NUMBER];
 };
--- watchpoint_system/watchpoint.h
+++ watchpoint_system/watchpoint.h
@@ -8,7 +8,7 @@
 #ifndef WATCHPOINT_H_
 #define WATCHPOINT_H_
 
-#include <stdint.h>		//contains int32_t
+#include <stdint.h>     //contains int32_t
 #include <map>
 #include "oracle_wp.h"
 
@@ -16,10 +16,10 @@
  *	Structure that contains all hardware emulation statistics for one thread
  */
 struct statistics_t {
-	long long checks;			//	total number of checks on faults
-	long long oracle_faults;	//	total number of times that the Oracle get faults
-	long long sets;				//	total number of times the API sets a watchpoint
-	long long updates;			//	total number of times the API updates a watchpoint
+	long long checks;          //	total number of checks on faults
+	long long oracle_faults;   //	total number of times that the Oracle get faults
+	long long sets;            //	total number of times the API sets a watchpoint
+	long long updates;         //	total number of times the API updates a watchpoint
 };
 
 /*
@@ -39,35 +39,35 @@ inline statistics_t operator +(const statistics_t &a, const statistics_t &b);
 template<class ADDRESS, class FLAGS>
 class WatchPoint {
 public:
-	WatchPoint();	//	Constructor
-	~WatchPoint();	//	Destructor
+	WatchPoint();  //	Constructor
+	~WatchPoint(); //	Destructor
 	
 	//Threading Calls
-	int start_thread(int32_t thread_id);	//	returns 0 on success, -1 on failure
-											//		(can't have multiple active threads holding the same thread_id)
-	int end_thread(int32_t thread_id);		//	turns an active thread into inactive state
-	void print_threads();					//	prints all active thread_id's
+	int start_thread(int32_t thread_id);   // returns 0 on success, -1 on failure
+                                          //    (can't have multiple active threads holding the same thread_id)
+	int end_thread(int32_t thread_id);     // turns an active thread into inactive state
+	void print_threads();                  // prints all active thread_id's
 	
 	//Reset Functions
-	void reset();							//	resets all threads, whether active or inactive
-	void reset(int32_t thread_id);			//	resets thread #thread_id if found, whether active or inactive
+	void reset();                          // resets all threads, whether active or inactive
+	void reset(int32_t thread_id);         // resets thread #thread_id if found, whether active or inactive
 	
 	//Checking Watchpoints
-	bool watch_fault	(ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);	//	check for r/w faults
-	bool read_fault		(ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);	//	only check r faults
-	bool write_fault	(ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);	//	only check w faults
+	bool watch_fault     (ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);   //	check for r/w faults
+	bool read_fault      (ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);   //	only check r faults
+	bool write_fault     (ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);   //	only check w faults
 	void print_watchpoints();
 	
 	//Changing Watchpoints
-	int set_watch		(ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);	//set	 11 (rw)
-	int set_read		(ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);	//set	 10
-	int set_write		(ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);	//set	 01
-	int rm_watch		(ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);	//set	 00
+	int set_watch        (ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);   //set    11 (rw)
+	int set_read         (ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);   //set    10
+	int set_write        (ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);   //set    01
+	int rm_watch         (ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);   //set    00
 	
-	int update_set_read	(ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);	//update 1x
-	int update_set_write(ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);	//update x1
-	int rm_read			(ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);	//update 0x
-	int rm_write		(ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);	//update x0
+	int update_set_read  (ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);   //update 1x
+	int update_set_write (ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);   //update x1
+	int rm_read          (ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);   //update 0x
+	int rm_write         (ADDRESS start, ADDRESS end, int32_t thread_id, bool ignore_statistics=false);   //update x0
 	
 	//Watchpoint Comparison Functions
 	/*
@@ -75,30 +75,30 @@ public:
 	 *	      every flag_thread2 watchpoints in thread_id2
 	 *	to see if any overlap exists.
 	 */
-	bool general_compare(int32_t thread_id1, int32_t thread_id2, FLAGS flag_thread1, FLAGS flag_thread2);
-	bool compare_rr		(int32_t thread_id1, int32_t thread_id2);
-	bool compare_rw		(int32_t thread_id1, int32_t thread_id2);
-	bool compare_wr		(int32_t thread_id1, int32_t thread_id2);
-	bool compare_ww		(int32_t thread_id1, int32_t thread_id2);
+	bool general_compare (int32_t thread_id1, int32_t thread_id2, FLAGS flag_thread1, FLAGS flag_thread2);
+	bool compare_rr      (int32_t thread_id1, int32_t thread_id2);
+	bool compare_rw      (int32_t thread_id1, int32_t thread_id2);
+	bool compare_wr      (int32_t thread_id1, int32_t thread_id2);
+	bool compare_ww      (int32_t thread_id1, int32_t thread_id2);
 	
 	//Hardware Emulation Statistic Functions
-	statistics_t get_statistics			(int32_t thread_id);
-	int			 set_statistics			(int32_t thread_id, statistics_t input);
-	statistics_t clear_statistics		();
-	void		 print_statistics		(bool active = false);
-	void		 print_statistics		(const statistics_t& to_print);
+	statistics_t get_statistics   (int32_t thread_id);
+	int          set_statistics   (int32_t thread_id, statistics_t input);
+	statistics_t clear_statistics ();
+	void         print_statistics (bool active = false);
+	void         print_statistics (const statistics_t& to_print);
 
 private:
 	//Data Structures (mainly maps from thread_id's to the thread's data)
-	map<int32_t, Oracle<ADDRESS, FLAGS> > 						oracle_wp;				//	for storing byte accurate emulation class
-	map<int32_t, statistics_t> 									statistics;				//	for storing all active threads and statistics
-	map<int32_t, statistics_t> 									statistics_inactive;	//	for storing all inactive threads and statistics (no overlaps)
+	map<int32_t, Oracle<ADDRESS, FLAGS> >                    oracle_wp;           //	for storing byte accurate emulation class
+	map<int32_t, statistics_t>                               statistics;          //	for storing all active threads and statistics
+	map<int32_t, statistics_t>                               statistics_inactive; //	for storing all inactive threads and statistics (no overlaps)
 	
 	//Useful iterators
-	typename map<int32_t, Oracle<ADDRESS, FLAGS> >::iterator 	oracle_wp_iter;
-	typename map<int32_t, Oracle<ADDRESS, FLAGS> >::iterator 	oracle_wp_iter_2;
-	map<int32_t, statistics_t>::iterator 						statistics_iter;
-	map<int32_t, statistics_t>::iterator 						statistics_inactive_iter;
+	typename map<int32_t, Oracle<ADDRESS, FLAGS> >::iterator oracle_wp_iter;
+	typename map<int32_t, Oracle<ADDRESS, FLAGS> >::iterator oracle_wp_iter_2;
+	map<int32_t, statistics_t>::iterator                     statistics_iter;
+	map<int32_t, statistics_t>::iterator                     statistics_inactive_iter;
 };
 
 #include "watchpoint.cpp"
