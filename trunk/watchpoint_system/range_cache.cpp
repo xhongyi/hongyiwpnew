7cf09a806aeb298650569aa497b46ad548959ea7
camellyx <camellyx@785ca939-0bf8-b47d-48db-cb9afb3264ec>
2011-07-11 22:15:30 +0000

    bug fixed in previous version; added assertion for off-chip bitmap

--- trunk/watchpoint_system/range_cache.cpp
+++ trunk/watchpoint_system/range_cache.cpp
@@ -2,9 +2,6 @@
 #define RANGE_CACHE_CPP_
 
 #include "range_cache.h"
-#include <iostream>
-
-using namespace std;
 
 template<class ADDRESS, class FLAGS>
 RangeCache<ADDRESS, FLAGS>::RangeCache(Oracle<ADDRESS, FLAGS> *wp_ref, bool ocbm_in, bool offcbm_in) {
@@ -66,6 +63,12 @@ int RangeCache<ADDRESS, FLAGS>::rm_watchpoint(ADDRESS start_addr, ADDRESS end_ad
 template<class ADDRESS, class FLAGS>
 int RangeCache<ADDRESS, FLAGS>::general_fault(ADDRESS start_addr, ADDRESS end_addr) {
    int rc_miss = 0;
+   // assert start_addr and end_addr is in the same page (same off-chip bitmap)
+   /*
+   if ((start_addr>>PAGE_OFFSET_LENGTH) != (end_addr>>PAGE_OFFSET_LENGTH)) {
+      cerr <<"off-chip bitmap error: asserting start and end in the same page. ";
+      assert(0);
+   }*/
    if (off_cbm && offcbm_wp->search_address(start_addr)->flags == WA_OFFCBM) {
                                                       // if off_cbm on, check if this range should be a off_cbm first
       typename std::deque< watchpoint_t<ADDRESS, FLAGS> >::iterator rc_write_iter, oracle_iter;
@@ -120,6 +123,12 @@ template<class ADDRESS, class FLAGS>
 int RangeCache<ADDRESS, FLAGS>::wp_operation(ADDRESS start_addr, ADDRESS end_addr, bool is_update) {
    int rc_miss = 0;
    if (off_cbm && offcbm_wp->search_address(start_addr)->flags == WA_OFFCBM) {
+      // assert start_addr and end_addr is in the same page (same off-chip bitmap)
+      /*
+      if ((start_addr>>PAGE_OFFSET_LENGTH) != (end_addr>>PAGE_OFFSET_LENGTH)) {
+         cerr <<"off-chip bitmap error: asserting start and end in the same page. ";
+         assert(0);
+      }*/
                                                       // if off_cbm on, check if this range should be a off_cbm first
       typename std::deque< watchpoint_t<ADDRESS, FLAGS> >::iterator rc_write_iter, oracle_iter;
       watchpoint_t<ADDRESS, FLAGS> temp;
@@ -142,6 +151,12 @@ int RangeCache<ADDRESS, FLAGS>::wp_operation(ADDRESS start_addr, ADDRESS end_add
       typename std::deque< watchpoint_t<ADDRESS, FLAGS> >::iterator rc_write_iter, oracle_iter;
       watchpoint_t<ADDRESS, FLAGS> temp;
       if (is_update) {
+         // assert start_addr and end_addr is in the same page (same off-chip bitmap)
+         /*
+         if ((start_addr>>PAGE_OFFSET_LENGTH) != (end_addr>>PAGE_OFFSET_LENGTH)) {
+            cerr <<"off-chip bitmap error: asserting start and end in the same page. ";
+            assert(0);
+         }*/
          // support for counting complex updates
          rc_write_iter = search_address(start_addr);
          if (rc_write_iter == rc_data.end())
