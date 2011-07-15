2b447b62f0c23dbf7f4d709d293197c8bde834c7
menacer@gmail.com <menacer@gmail.com@785ca939-0bf8-b47d-48db-cb9afb3264ec>
2011-07-15 05:29:44 +0000

    Ability, in the parser script, to only print stats that exist in.

--- utils/output_parser.pl
+++ utils/output_parser.pl
@@ -25,6 +25,14 @@ my $range_kickouts;
 my $range_ocbm_misses;
 my $range_ocbm_kickouts;
 
+my $have_seen_vm;
+my $have_seen_mt_vm;
+my $have_seen_memtracker;
+my $have_seen_mmp_sst;
+my $have_seen_mmp_mlpt;
+my $have_seen_rc;
+my $have_seen_rc_ocbm;
+
 if(scalar @ARGV != 1) {
     print "Incorrect calling method.\n";
     print "Shold be $0 {input file}\n";
@@ -60,36 +68,46 @@ while(<READFILE>)
     }
     elsif(m/Page table \(multi\) faults/) {
         $vm_faults += $split_fields[-1];
+        $have_seen_vm = 1;
     }
     elsif(m/Page table \(multi\) bitmap/) {
         $vm_bitmap_changes += $split_fields[-1];
+        $have_seen_vm = 1;
     }
     elsif(m/Page table \(single\) faults/) {
         $multi_thread_vm_faults += $split_fields[-1];
+        $have_seen_mt_vm = 1;
     }
     elsif(m/Page table \(single\) bitmap/) {
         $multi_thread_vm_bitmap_changes += $split_fields[-1];
+        $have_seen_mt_vm = 1;
     }
     elsif(m/MemTracker read/) {
         $memtracker_tl1_misses += $split_fields[-1];
+        $have_seen_memtracker = 1;
     }
     elsif(m/MemTracker write/) {
         $memtracker_write_misses += $split_fields[-1];
+        $have_seen_memtracker = 1;
     }
     elsif(m/total wp write size/) {
         $memtracker_bitmap_writes += $split_fields[-1];
     }
     elsif(m/Range Cache \(single\) read miss/ || m/Range Cache \(single\) write miss/) {
         $range_misses += $split_fields[-1];
+        $have_seen_rc = 1;
     }
     elsif(m/Range Cache \(single\) kickouts dirty/) {
         $range_kickouts += $split_fields[-1];
+        $have_seen_rc = 1;
     }
     elsif(m/Range Cache \(OCBM\) read miss/ || m/Range Cache \(OCBM\) write miss/) {
         $range_ocbm_misses += $split_fields[-1];
+        $have_seen_rc_ocbm = 1;
     }
     elsif(m/Range Cache \(OCBM\) kickouts dirty/) {
         $range_ocbm_kickouts += $split_fields[-1];
+        $have_seen_rc_ocbm = 1;
     }
 }
 
@@ -97,15 +115,25 @@ print "Number of instructions: " . $instruction_count . "\n";
 print "Total memory ops: " . $total_memory_ops . "\n";
 print "Shared memory ops: " . $shared_mem_ops . "\n";
 print "Number of sets/removes: " . $watchpoint_sets_and_removes . "\n";
-print "VM False Faults: " . ($vm_faults-$shared_mem_ops) . "\n";
-print "VM Bitmap changes: " . $vm_bitmap_changes . "\n";
-print "Per-Thread VM Faults: " . ($multi_thread_vm_faults-$shared_mem_ops) . "\n";
-print "Per-Thread VM Bitmap changes: " . $multi_thread_vm_bitmap_changes . "\n";
-print "MemTracker TL1 Read Misses: " . $memtracker_tl1_misses . "\n";
-print "MemTracker TL1 Write Misses: " . $memtracker_write_misses . "\n";
-print "MemTracker Total TL1 Misses: " . ($memtracker_tl1_misses + $memtracker_write_misses) . "\n";
-print "MemTracker Bitmap Writes: " . $memtracker_bitmap_writes . "\n";
-print "Range Cache misses: " . $range_misses . "\n";
-print "Range Cache kickouts: " . $range_kickouts . "\n";
-print "Range Cache + OCBM misses: " . $range_ocbm_misses . "\n";
-print "Range Cache + OCBM kickouts: " . $range_ocbm_kickouts . "\n";
+if($have_seen_vm==1) {
+    print "VM False Faults: " . ($vm_faults-$shared_mem_ops) . "\n";
+    print "VM Bitmap changes: " . $vm_bitmap_changes . "\n";
+}
+if($have_seen_mt_vm==1) {
+    print "Per-Thread VM Faults: " . ($multi_thread_vm_faults-$shared_mem_ops) . "\n";
+    print "Per-Thread VM Bitmap changes: " . $multi_thread_vm_bitmap_changes . "\n";
+}
+if($have_seen_memtracker==1) {
+    print "MemTracker TL1 Read Misses: " . $memtracker_tl1_misses . "\n";
+    print "MemTracker TL1 Write Misses: " . $memtracker_write_misses . "\n";
+    print "MemTracker Total TL1 Misses: " . ($memtracker_tl1_misses + $memtracker_write_misses) . "\n";
+    print "MemTracker Bitmap Writes: " . $memtracker_bitmap_writes . "\n";
+}
+if($have_seen_rc==1) {
+    print "Range Cache misses: " . $range_misses . "\n";
+    print "Range Cache kickouts: " . $range_kickouts . "\n";
+}
+if($have_seen_rc_ocbm==1) {
+    print "Range Cache + OCBM misses: " . $range_ocbm_misses . "\n";
+    print "Range Cache + OCBM kickouts: " . $range_ocbm_kickouts . "\n";
+}
